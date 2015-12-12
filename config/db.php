e33d0798b81ae3f6002aa45c1cfbbcb88fa50e46
Muhammad Khayat <khayat@infest.or.id>
2015-12-12 13:32:20 +0700

    env

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=pantaudesa',
     'username' => 'root',
-    'password' => '',
+    'password' => 'merdesa',
     'charset' => 'utf8',
 ];
--- config/web.php
+++ config/web.php
@@ -32,6 +32,7 @@ $config = [
         ],
         'urlManager' => [
             'enablePrettyUrl' => true,
+	    'showScriptName' => false,
             'rules' => [
             ],
         ],
